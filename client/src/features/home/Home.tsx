169e4c8c586972c32a967ad32fb0489d876fcbd9
Mikael Andres <msandres123@gmail.com>
2022-06-20 15:21:27 -0400

    minor details

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../app/store/configureStore';
 
 export default function Home() {
+  const { user }: any = useAppSelector((state) => state.account);
+
   return (
     <div className='home'>
         <h1>Hello</h1>
         <p>Welcome to Grat-Culator, a way to track the gratuities you earn.</p>
         <Image className='d-block mx-auto' src='/images/Funny-Restaurant-memes-20.jpeg' alt="not found" height="290"/>
+        {user && <Link className='home-link'  to="shift-collection"><p>View your shifts</p></Link> }
     </div>
   )
 }
